cbedcca43aad09ea747fa012406dd10c79d540bc
Allen Chang <AllenChang@admins-macbook-pro.local>
2016-10-05 17:27:33 +0800

    Initial Commit

---
+++ testupdatetogithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testupdatetogithub
+//
+//  Created by AllenChang on 2016/10/5.
+//  Copyright © 2016年 AllenChang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
